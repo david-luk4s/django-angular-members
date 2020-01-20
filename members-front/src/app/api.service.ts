4f4170b4333455969cfa9fb6d07902d46d2a9d40
david-luk4s <davidlucadl99@gmail.com>
2020-01-20 12:05:55 -0500

    Add news tokens

--- members-front/src/app/api.service.ts
+++ members-front/src/app/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   baseUrl = 'http://127.0.0.1:8000/'
-  token = 'Token 293b771313e82aaf95009d49619e88a610f94ede';
+  token = 'Token 87e1925000dc2c11f20b7a5957b37e7642d96b72';
   httpHeaders = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', this.token);
 
   constructor(private requests: HttpClient) { }
