3bc0f7a118c5c9b9eb7ccd9d33cb0a1a49b8edae
david-luk4s <davidlucadl99@gmail.com>
2020-01-20 15:22:59 -0500

    Config databaset setting

--- members-back/front/settings.py
+++ members-back/front/settings.py
@@ -87,6 +87,14 @@ WSGI_APPLICATION = 'front.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangomembers',
+        'USER': 'django',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
